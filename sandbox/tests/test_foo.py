59da28ea2597f5a4813eb91be7e22d2356f4f3ef
Alvin Chow <alvin@doordash.com>
2014-03-06 10:09:01 -0800

    another db test

--- sandbox/tests/test_foo.py
+++ sandbox/tests/test_foo.py
@@ -3,9 +3,11 @@ import pytest
 def test_foo():
   assert 1== 1
 
+from sandbox.apps.corgi.models import Corgi
+
 @pytest.mark.django_db
 def test_db():
-  from sandbox.apps.corgi.models import Corgi
+
   assert Corgi.objects.count() == 0
   corgi = Corgi(name='Mollie', weight=25,length=15, description='Best corgi')
   corgi.save()
@@ -14,3 +16,11 @@ def test_db():
   corgi =  Corgi.objects.get(name='Mollie')
   assert corgi.length == 15
   assert corgi.description == 'Best corgi'
+
+@pytest.mark.django_db
+def test_again():
+
+  assert Corgi.objects.count() == 0
+  corgi = Corgi(name='Benny')
+  corgi.save()
+  assert Corgi.objects.count() == 1
