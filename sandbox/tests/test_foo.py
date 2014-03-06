bc1e86ff1923fb910ba01d369e0935cbe341dfdf
Alvin Chow <alvin@doordash.com>
2014-03-06 10:05:37 -0800

    mod test for migration

--- sandbox/tests/test_foo.py
+++ sandbox/tests/test_foo.py
@@ -7,8 +7,10 @@ def test_foo():
 def test_db():
   from sandbox.apps.corgi.models import Corgi
   assert Corgi.objects.count() == 0
-  corgi = Corgi(name='Mollie', weight=25)
+  corgi = Corgi(name='Mollie', weight=25,length=15, description='Best corgi')
   corgi.save()
 
   assert Corgi.objects.count() == 1
-  print Corgi.objects.get(name='Mollie').weight
+  corgi =  Corgi.objects.get(name='Mollie')
+  assert corgi.length == 15
+  assert corgi.description == 'Best corgi'
